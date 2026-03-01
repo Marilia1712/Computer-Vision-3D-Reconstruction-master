ce72273f6cd310cf80b4af327953d4d8618a8d65
Marilia Merendi <mariliamerendi@gmail.com>
2026-03-01 21:13:32 +0100

    cam position and cam rotation functions implemented (for visualization)

--- assignment.py
+++ assignment.py
@@ -17,7 +17,38 @@ def generate_grid(width, depth):
             data.append([x*block_size - width/2, -block_size, z*block_size - depth/2])
             colors.append([1.0, 1.0, 1.0] if (x+z) % 2 == 0 else [0, 0, 0])
     return data, colors
-   
+
+def get_cam_positions():
+    """
+    Generates camera locations at the 4 corners
+    of the room based on the locations estimated in Task 1
+    """
+    cam_pos_list = []
+
+    for i in range(1,5):
+        cam_pos_list.append([cam_pos[f'cam{i}'][0], cam_pos[f'cam{i}'][1], cam_pos[f'cam{i}'][2]]) 
+
+    return [cam_pos_list, \
+        [[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0], [1.0, 1.0, 0]]]
+
+def get_cam_rotation_matrices():
+    """"
+    Generates camera rotation matrices based on the
+    orientations estimated in Task 1
+    """
+    cam_rot_list = []
+
+    for i in range(1,5):
+        R = cam_R[f'cam{i}']
+
+        #4x4 matrix is needed for rendering
+        mat4= np.eye(4)
+        mat4[:3,:3] = R
+        cam_rot_list.append(mat4)
+    
+    return cam_rot_list
+
+
 def set_voxel_positions(width, height, depth):
     # Generates random voxel locations
     # TODO: You need to calculate proper voxel arrays instead of random ones.
@@ -29,26 +60,6 @@ def set_voxel_positions(width, height, depth):
                     data.append([x*block_size - width/2, y*block_size, z*block_size - depth/2])
                     colors.append([x / width, z / depth, y / height])
     return data, colors
-
-def get_cam_positions():
-    # Generates dummy camera locations at the 4 corners of the room
-    # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
-    return [[-64 * block_size, 64 * block_size, 63 * block_size],
-            [63 * block_size, 64 * block_size, 63 * block_size],
-            [63 * block_size, 64 * block_size, -64 * block_size],
-            [-64 * block_size, 64 * block_size, -64 * block_size]], \
-        [[1.0, 0, 0], [0, 1.0, 0], [0, 0, 1.0], [1.0, 1.0, 0]]
-
-def get_cam_rotation_matrices():
-    # Generates dummy camera rotation matrices, looking down 45 degrees towards the center of the room
-    # TODO: You need to input the estimated camera rotation matrices (4x4) of the 4 cameras in the world coordinates.
-    cam_angles = [[0, 45, -45], [0, 135, -45], [0, 225, -45], [0, 315, -45]]
-    cam_rotations = [glm.mat4(1), glm.mat4(1), glm.mat4(1), glm.mat4(1)]
-    for c in range(len(cam_rotations)):
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][0] * np.pi / 180, [1, 0, 0])
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][1] * np.pi / 180, [0, 1, 0])
-        cam_rotations[c] = glm.rotate(cam_rotations[c], cam_angles[c][2] * np.pi / 180, [0, 0, 1])
-    return cam_rotations
 
 
 """
@@ -162,9 +173,9 @@ for i in range(1,5):
     background = background_detection(video_path_back)
     foreground = background_subtraction(video_path_fore, background, kernel)
 
-    """
-    Exercise 3
-    """
+"""
+Exercise 3
+"""
 
 # Get camera position (adaptation of code from Assingnment 1)
 
@@ -209,6 +220,3 @@ for i in range(1, 5):
 
     #Store params for each camera
     params[f'cam{i}'] = [camera_matrix, dist, rvec, tvec]
-
-
-
